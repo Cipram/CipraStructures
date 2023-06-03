a90c9588610a65be7f20d8e6b820e7a33f6c40ea
Cipriano Martin <cipriamartinf@gmail.com>
2023-06-03 14:42:09 -0300

    implementando ABB

---
+++ src/TDAArbolBB/MiComparator.java
@@ -0,0 +1,12 @@
+package TDAArbolBB;
+
+public class MiComparator<E> implements java.util.Comparator<E> {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public int compare(E o1, E o2) {
+		return ((Comparable<E>) o1).compareTo(o2);
+	}
+	
+
+}
